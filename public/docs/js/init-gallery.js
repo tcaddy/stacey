7a7c23dfd21cfd215d8d388f784d928c42e6ff01
Sven Lito <me@svenlito.com>
2011-01-16 01:25:28 +0000

    cached .image selector

--- public/docs/js/init-gallery.js
+++ public/docs/js/init-gallery.js
@@ -1,13 +1,15 @@
 $(function() {
-
+  var image = $('.image');
+  
   // don't init gallery if it consists of a single photo
-  if($("div.image").length > 1) {
+  if (image.length > 1) {
+    
     // wrap images
-    $("div.image").wrapAll("<div id='image-wrapper'><div id='image-holder'></div></div>");
-    $("div.image").show();
+    image.wrapAll("<div id='image-wrapper'><div id='image-holder'></div></div>");
+    image.show();
     
     // init gallery: Gallery.init(imageHolder, imageWrapperWidth, imageCountHolder, nextButton, prevButton)
-    Gallery.init($("div#image-holder"), 560, $("p#gallery-count").children("span")[0], $("a#next-image"), $("a#previous-image"));
+    Gallery.init($("#image-holder"), 560, $("p#gallery-count").children("span")[0], $("a#next-image"), $("a#previous-image"));
 
   } else {
     // hide gallery count and navigation
