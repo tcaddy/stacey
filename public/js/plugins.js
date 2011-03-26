a883f040b4d55094d119d581e2db7200fe0e6f10
Teddy Caddy <info@teddycaddy.com>
2011-03-26 10:29:00 -0400

    add jQuery.prettyDate() plugin:  * copied from / based on CouchApp  * will be used for timestamp of Tweets

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -81,3 +81,45 @@ Gallery = {
     }, { duration: 600, queue: false });
   }
 }
+
+/* prettyDate() is copied from CouchApp */
+jQuery.fn.prettyDate = function() {
+  jQuery(this).each(function() {
+    var string, title = jQuery(this).attr("title");
+    if (title) {
+      string = jQuery.prettyDate(title);
+    } else {
+      string = jQuery.prettyDate(jQuery(this).text());
+    }
+    jQuery(this).text(string);
+  });
+};
+
+jQuery.prettyDate = function(time){
+  var date;
+  try {
+    date = new Date(time);
+  } catch(e) {
+    date=null;
+  }
+  if (!(!!(date && date.getTimezoneOffset && date.setUTCFullYear))) { // copied for underscore.js isDate()
+	  date = new Date(time.replace(/-/g,"/").replace("T", " ").replace("Z", " +0000").replace(/(\d*\:\d*:\d*)\.\d*/g,"$1")) ;
+	}
+	var diff = (((new Date()).getTime() - date.getTime()) / 1000),
+		day_diff = Math.floor(diff / 86400);
+
+  if (isNaN(day_diff)) return time;
+
+	return day_diff < 1 && (
+			diff < 60 && "just now" ||
+			diff < 120 && "1 minute ago" ||
+			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+			diff < 7200 && "1 hour ago" ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+		day_diff == 1 && "yesterday" ||
+		day_diff < 21 && day_diff + " days ago" ||
+		day_diff < 45 && Math.ceil( day_diff / 7 ) + " weeks ago" ||
+    time;
+    // day_diff < 730 && Math.ceil( day_diff / 31 ) + " months ago" ||
+    // Math.ceil( day_diff / 365 ) + " years ago";
+};
