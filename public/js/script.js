d9340e80673577c4b9fa2886af0007a1c850025a
Teddy Caddy <info@teddycaddy.com>
2011-03-27 22:36:19 -0400

    change Google Analytic syntax in TC.setup_gal()

--- public/js/script.js
+++ public/js/script.js
@@ -48,11 +48,15 @@ var TC = TC || {}; // my initials; namespace for my js code
   }
   
   TC.setup_gal = function() {
-    //mathiasbynens.be/notes/async-analytics-snippet Change UA-XXXXX-X to be your site's ID 
-    var _gaq=[['_setAccount',TC.gal.key],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', TC.gal.key]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
   }
 
   TC.setup_gallery = function() {
