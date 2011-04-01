0245996e5ea789f0f1a57b16886875c49411df7a
Teddy Caddy <info@teddycaddy.com>
2011-03-31 22:13:34 -0400

    load Twitter @Anywhere via javascript after page load instead of in   a <script> tag in footer add underscore.js and include in footer

--- public/js/script.js
+++ public/js/script.js
@@ -26,32 +26,77 @@ s.parentNode.insertBefore(g,s)}(document,'script'));
   
   $(function(){
     TC.setup_twitter();
+    if (!UnderScore.isUndefined(TC.twitter)) {
+      TC.setup_twitter();
+      UnderScore.delay(TC.ensure_twitter_setup,100);
+    }
   });
   
-  TC.setup_twitter = function() {
-    TC.twitter.status_template = '<tr class="status"><td class="avatar"><img alt="{{screenName}}" src="{{image_url}}"/></td><td class="screen_name">@{{screenName}}</td><td class="status">{{text}}</td></tr>';
-    TC.twitter.status_simple_template = '<tr class="status"><td class="status">{{text}}<span class="timestamp">{{timestamp}}</span></td></tr>';
+  TC.ensure_twitter_setup = function() {
+    if (!TC.twitter_setup_complete) {
+      TC.twitter_anywhere();
+    }
+  }
 
-    $("#timeline .busy:hidden").show();
-    twttr.anywhere(function (T) {
-      T.hovercards();
-      T("#follow-placeholder").followButton(TC.twitter.handle);
-      if($("#timeline").length>0) {
-        T.User.find(TC.twitter.handle).timeline().each(function(status) {
-          $("table#timeline.caption_rounded_bottom").removeClass("caption_rounded_bottom");
-          $("#timeline .busy:visible").hide();
-          $("#timeline").append(
-            Mustache.to_html(TC.twitter.status_simple_template, {
-              text: status.text,
-              timestamp: status.createdAt
-            })
-          );
-          $("#timeline .timestamp:last").prettyDate();
-          T("#timeline td.status").hovercards();
-        });
-        UnderScore.delay(TC.fix_webkit_captions,500);
+  TC.setup_twitter = function() {
+    TC.twitter.script_src_tag_written = false;
+    TC.twitter_setup_complete = false;
+    TC.setup_twitter_anywhere_script();
+  }
+
+  TC.twitter_anywhere = function() {
+    if (TC.twitter_anywhere_script_test()) {
+      TC.twitter_setup_complete = true;
+      TC.twitter.status_template = '<tr class="status"><td class="avatar"><img alt="{{screenName}}" src="{{image_url}}"/></td><td class="screen_name">@{{screenName}}</td><td class="status">{{text}}</td></tr>';
+      TC.twitter.status_simple_template = '<tr class="status"><td class="status">{{text}}<span class="timestamp">{{timestamp}}</span></td></tr>';
+
+      $("#timeline .busy:hidden").show();
+      twttr.anywhere(function (T) {
+        if($("#timeline").length>0) {
+          T.User.find(TC.twitter.handle).timeline().each(function(status) {
+            $("table#timeline.caption_rounded_bottom").removeClass("caption_rounded_bottom");
+            $("#timeline .busy:visible").hide();
+            $("#timeline").append(
+              Mustache.to_html(TC.twitter.status_simple_template, {
+                text: status.text,
+                timestamp: status.createdAt
+              })
+            );
+            $("#timeline .timestamp:last").prettyDate();
+            T("#timeline td.status").hovercards();
+          });
+          UnderScore.delay(TC.fix_webkit_captions,500);
+        }
+        T.hovercards();
+        T("#follow-placeholder").followButton(TC.twitter.handle);
+      });
+    } else {
+      UnderScore.delay(TC.ensure_twitter_setup,100);
+    }
+  }
+
+  TC.setup_twitter_anywhere_script = function() {
+    if (!TC.twitter_anywhere_script_test()) {
+      if (!TC.twitter.script_src_tag_written) {
+        $("head").append(
+          '<script src="http://platform.twitter.com/anywhere.js?id='+TC.twitter.api_key+'" type="text/javascript"></script>'
+        );
+        TC.twitter.script_src_tag_written = true;
       }
-    });
+      $("head script[src='http://platform.twitter.com/anywhere.js?id="+TC.twitter.api_key+"']").load(function(e){
+        TC.twitter_anywhere();
+      });
+    } else {
+      TC.twitter_anywhere();
+    }
+    return null;
+  }
+
+  TC.twitter_anywhere_script_test = function() {
+    try {
+      return ((!UnderScore.isUndefined(twttr)) && (typeof(twttr)!=='undefined') && UnderScore.isFunction(twttr.anywhere));
+    } catch(e) {}
+    return false;
   }
   
   TC.fix_webkit_captions = function() {
